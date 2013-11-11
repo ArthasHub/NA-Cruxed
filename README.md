595bbeea48f1a01743a020156f5ae37e8d7fdeb5
ArthasHub <zxs867179@gmail.com>
2013-11-11 12:35:18 +0800

    初次建立，练习分支

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ NA-Cruxed
 =========
 
 This is my persional dream about code. It may be anything.
+
+版本日记：
+v 0.0.1版
---
+++ js/cruxed.js
@@ -0,0 +1,15 @@
+//~cruxed.js
+/*
+*
+*提交时间L2013年11月8日15:37:28
+*作者：Arthas
+*作用：封装了NA-Cruxed的核心代码
+*
+*/
+
+(function(window , undefined){
+	document = window.document;
+	version = "0.0.1";
+	location = window.location;
+})(window);
+//~cruxed.js~
